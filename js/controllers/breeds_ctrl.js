76c1fce16a340db40976f23e358b16f20ecc400a
Christian Carey <christianrcarey@gmail.com>
2017-01-24 16:25:46 -0500

    search for puppies

--- js/controllers/breeds_ctrl.js
+++
@@ -1,9 +0,0 @@
-PuppiesAPI.controller('BreedsCtrl', ['$scope', 'breeds',
-  function($scope, breeds) {
-    $scope.breeds = [];
-
-    breeds.sendGetRequest().then(function(response) {
-      angular.copy(response.data, $scope.breeds);
-    });
-  }
-]);
--- js/controllers/puppies_ctrl.js
+++ js/controllers/puppies_ctrl.js
@@ -1,10 +1,15 @@
-PuppiesAPI.controller('PuppiesCtrl', ['$scope', 'puppies',
-  function($scope, puppies) {
+PuppiesAPI.controller('PuppiesCtrl', ['$scope', 'puppies', 'breeds',
+  function($scope, puppies, breeds) {
     $scope.puppies = [];
+    $scope.breeds = [];
 
     puppies.sendGetRequest().then(function(response) {
-      console.log(response); 
+      console.log(response.data); 
       angular.copy(response.data, $scope.puppies);
+    });
+
+     breeds.sendGetRequest().then(function(response) {
+      angular.copy(response.data, $scope.breeds);
     });
   }
 ]);
